7d6be5a6db55d0a2ef658cadfb668b2b75566f34
joe-lemay <151972492+joe-lemay@users.noreply.github.com>
2023-11-25 11:30:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let burgerBtn = document.querySelector("#burger-btn");
+console.log(burgerBtn)
+let hamburger = document.querySelector("#hamburger");
+let burgerToggle = true;
+let messageFormInputs = document.querySelectorAll(".form-input");
+
+function toggleBurger() {
+    console.log(hamburger.classList)
+    console.log(burgerToggle)
+    burgerToggle = !burgerToggle;
+    if(burgerToggle == false){
+        hamburger.classList.remove("ham-hide");
+    }else{
+        hamburger.classList.add("ham-hide");
+    }
+}
+burgerBtn.addEventListener("click", toggleBurger);
+
+function clearForm(){
+    for( let input of messageFormInputs){
+        input.value = "";
+    }
+}
